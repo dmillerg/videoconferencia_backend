ade16226a64a993010c03e8f54e501c0dcc36f61
Daniel <dmillergg@gmail.com>
2022-09-26 20:01:20 -0400

    asjutes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,19 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { Token } from "./entity/token.entity";
+import { Usuario } from "./entity/usuario.entity";
+import { VideoConferencia } from "./entity/videoconferencia.entity";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "root",
+    password: "",
+    database: "videoconferencia",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [Usuario, VideoConferencia, Token],
     migrations: [],
     subscribers: [],
 })
